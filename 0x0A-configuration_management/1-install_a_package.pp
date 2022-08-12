1374ea3f0709536ea817173006c8692771f01e7a
PauliCrespi <4377@holberton.com>
2022-08-12 13:48:58 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #install flask from pip3
 
-exec { 'install python packages':
-  command => 'pip3 install flask flask_restful apiai',
-  path    => ['/usr/bin/'],
-  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
