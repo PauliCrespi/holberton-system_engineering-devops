524fa88a92b055d70da2242e86703364ebd44202
PauliCrespi <4377@holberton.com>
2022-08-12 13:30:11 -0700

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#create file
+
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
