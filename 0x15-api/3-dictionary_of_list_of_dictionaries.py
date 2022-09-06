90b01fd5b8cb5d276c3c9d5a65c9c01cd799838b
root <4377@holbertonschool.com>
2022-09-06 15:35:59 -0700

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,19 +7,20 @@ import requests
 if __name__ == "__main__":
     todos = []
     dictionary = {}
-    api_url = "https://jsonplaceholder.typicode.com/users/"
+    api_url = "https://jsonplaceholder.typicode.com/users"
     resp_u = requests.get(api_url).json()
     for user in resp_u:
+        todos = []
         idd = user.get('id')
         api_t = "https://jsonplaceholder.typicode.com/todos"
         resp_t = requests.get(api_t, params={"userId": idd}).json()
+        item = ""
         for item in resp_t:
             list_t = {}
             list_t["task"] = item.get('title')
             list_t["completed"] = item.get('completed')
             list_t["username"] = user.get('username')
             todos.append(list_t)
-        idd = user.get('id')
         dictionary[idd] = todos
     with open('todo_all_employees.json', 'w+') as f:
         json.dump(dictionary, f)
