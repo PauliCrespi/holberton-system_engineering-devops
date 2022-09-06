3ea564964ced830677d5170612898577eaf9b8a0
root <4377@holbertonschool.com>
2022-09-06 14:48:43 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/users/" + str(argv[1])
     api_t = "https://jsonplaceholder.typicode.com/todos"
     resp_t = requests.get(api_t, params={"userId": argv[1]}).json()
-    resp_u = requests.get(api_url)
+    resp_u = requests.get(api_url).json()
     idd = argv[1]
     for item in resp_t:
         list_t = {}
         list_t["task"] = item.get('title')
-        list_t["completed"] = str(item.get('completed'))
-        list_t["username"] = resp_u.json()["username"]
+        list_t["completed"] = item.get('completed')
+        list_t["username"] = resp_u.get('username')
         todos.append(list_t)
     dictionary = {}
     dictionary[idd] = todos
