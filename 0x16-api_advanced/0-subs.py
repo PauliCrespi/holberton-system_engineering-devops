86911f2531859ffa9c1de7cf5edd2657348dbc5c
root <4377@holbertonschool.com>
2022-09-22 06:59:58 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""task 0"""
+""" task 0 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """number of suscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    cont = '(Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    cont2 = ' (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-    user_agent = {'User-agent': 'Mozilla/5.0 {}{}'.format(cont, cont2)}
-    response = requests.get(url, headers=user_agent)
+    user = {"User-Agent": "My User Agent 1.0"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
 
-    if response.status_code != 200:
+    if request.status_code != 200:
         return 0
     else:
-        return response.json().get("data").get("suscribers")
+        return request.json().get("data").get("subscribers")
