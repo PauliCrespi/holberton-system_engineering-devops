512715d02436059a6e1969fc87766f92e64a8782
root <4377@holbertonschool.com>
2022-09-22 06:26:16 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """number of suscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     user_agent = {'User-agent': 'Mozilla/5.0'}
-    response  = requests.get(url, headers = user_agent)
+    response = requests.get(url, headers=user_agent)
 
     if response.status_code != 200:
         return 0
