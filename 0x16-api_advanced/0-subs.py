996cbbb49a13b5d0fe1e5acaabd81d0b349c0339
root <4377@holbertonschool.com>
2022-09-22 06:44:27 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """number of suscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = {'User-agent': 'Mozilla/5.0'}
+    cont = '(Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    cont2 = ' (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    user_agent = {'User-agent': 'Mozilla/5.0 {}{}'.format(cont, cont2)}
     response = requests.get(url, headers=user_agent)
 
     if response.status_code != 200:
